188ecc30c453855abbc2a0298d24bd8f04399c70
Evan Carlin <evan@carlin.com>
2017-03-22 11:30:14 -0700

    Taking in user input and can perform serial

--- threadedMatrixMultiplication/main.c
+++ threadedMatrixMultiplication/main.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
 #include "matrixMultiplication.h"
+#define usage "Usage: ./mmm <mode> [num threads] <size>"
 
-int main(){
-	int **m1 = generateRandomMatrix(3);
-	int **m2 = generateRandomMatrix(3);
+int main(int argc, char *argv[]){
+	struct timezone Tzp;
+	struct timeval Tp;
 
-	printf("M1\n");
-	printMatrix(m1, 3, 3);
-
-	printf("\nM2\n");
-	printMatrix(m2, 3, 3);
+    gettimeofday (&Tp, &Tzp);
+    printf("start %f\n", Tp.tv_sec + Tp.tv_usec*1.0e-6);
 
 
-	int **result = matrixMultiplication(m1, m2, 2, 3, 3);
-	printf("\nResult\n");
-	printMatrix(result, 1, 3);
+
+	//Want to do serial multiplication
+	if(argc == 3 && strcmp(argv[1], "S") == 0){
+		printf("Serial\n");
+		printf("%s\n", argv[2]);
+		int dim = atoi(argv[2]);
+		printf("%d\n", dim);
+
+		//Creates the matrices to multiply together
+		int **m1 = generateRandomMatrix(dim);
+		int **m2 = generateRandomMatrix(dim);
+
+		int **result = matrixMultiplication(m1, m2, 0, dim, dim);
+		printf("\nResult\n");
+		printMatrix(result, dim, dim);
+
+		gettimeofday (&Tp, &Tzp);
+		printf("end %f\n", Tp.tv_sec + Tp.tv_usec*1.0e-6);
+
+		exit(1);
+	}
+
+	else{
+		printf("%s\n", usage);
+		exit(1);
+	}
 
 	return 1;
 }
